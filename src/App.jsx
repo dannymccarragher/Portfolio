d713d58282aeedec7d0a905c3ab6db5a63192613
Danny McCarragher <dannymccarragher@gmail.com>
2025-10-17 15:10:33 -0700

    dock v1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Research from './components/Research'
 import Experience from './components/Experience'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
+import { Dock, DockIcon } from './components/ui/dock'
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
       <Research />
       <Skills />
       <Projects />
+      <Dock />
     </>
   )
 }
